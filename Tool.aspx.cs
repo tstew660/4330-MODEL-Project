bb5b61d7e6ebc3d40fcf6c7ee2ec3ddcedcc9309
Kamryn Gamble <33157590+kamryngamble@users.noreply.github.com>
2018-04-14 09:51:51 -0700

    Add files via upload
    Tool's "# hours each technician is idle" functionality

---
+++ Tool.aspx.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Xml;
+
+namespace _4330_MODEL_Project
+{
+    public partial class About : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+    public class AssessmentTool
+    {
+        public double jobWaitTime;
+        public double queueLength;
+        public int jobsNotAddressed;
+        public double percentQueueEmpty;
+        public double techHoursIdle;
+
+        protected void  dailyReport()
+        {
+            //looping through techs, printing name and hours idle
+            XmlDocument techs = new XmlDocument();
+            techs.Load(HttpContext.Current.Server.MapPath("~/Technician.xml"));
+            
+
+        }
+
+        protected void monthlyReport()
+        {
+
+        }
+    }
+
+    
+
+
+}
